74e21979ffd832a9808f90d3f4678a03c36185d5
cjb543 <cjb543@protonmail.com>
2024-12-03 00:58:34 -0800

    Refactor directories and update in code

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import './src/input.css'
+import '../src/styles/input.css'
 
 // Fix page reload/back errors
 window.addEventListener('popstate', function(event) { window.location.reload(); });
 
-//Makes web page 404 if invalid link is entered. Website routes are hardcoded.
+// Makes web page 404 if invalid link is entered. Website routes are hardcoded.
 window.addEventListener('load', () => {
     const validRoutes = ['/', '/about', '/contact', '/privacypolicy',
                          '/ege', '/receiving', '/warehouse', '/careers',
